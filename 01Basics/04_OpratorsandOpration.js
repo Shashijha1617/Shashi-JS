69b1bdb0ee4175ce92b3e0175a47fc7d2cef5cc9
SHASHI KUMAR JHA <130572406+Shashijha1617@users.noreply.github.com>
2024-02-16 21:34:38 +0000

    DataType Summary

---
+++ 01Basics/04_OpratorsandOpration.js
@@ -0,0 +1,27 @@
+let value1 = 2
+let value2 = 3
+let value3 = "43"
+let value4 = "5a"
+
+console.log(value1 + value2);
+console.log(value1 - value2);
+console.log(value1 * value2);
+console.log(value1 / value2);
+console.log(value1 ** value2);
+console.log(value1 % value2);
+
+console.log(value1 + value3);
+console.log(value1 - value3);
+console.log(value1 * value3);
+console.log(value1 / value3);
+console.log(value1 ** value3);
+console.log(value1 % value3);
+
+console.log(typeof (value1 / value3));
+
+console.log(value1 + value4);
+console.log(value1 - value4);
+console.log(value1 * value4);
+console.log(value1 / value4);
+console.log(value1 ** value4);
+console.log(value1 % value4);
---
+++ DataTypes-summary.js
@@ -0,0 +1,59 @@
+// Premitive Data Types
+/*
+7 Types of Premetive Data Types
+01 : string
+02 : number
+03 : boolen
+04 : null
+05 : bigInt
+06 : symbol
+07 : undefined
+*/
+// let Score = 100
+// let ScoreValue = 100.50
+
+// let myname = "shashi Kumar Jha"
+// let isVisible = true
+// let userEmail;
+// let temprature = null
+// let id = Symbol("123")
+// let anotherID = Symbol("123")
+// let bigNumber = 21148454454212154454874577454754545n
+
+
+
+// console.log(typeof Score);
+// console.log(typeof ScoreValue);
+// console.log(typeof myname);
+// console.log(typeof isVisible);
+// console.log(typeof userEmail);
+// console.log(typeof temprature);
+// console.log(typeof anotherID);
+// console.log(typeof bigNumber);
+// console.log(id === anotherID);
+
+
+
+
+// Non- Premetive Data Types or Refrence Type
+/*
+3 Types of Refrence Type Database
+01 : Array
+02 : Object
+03 : Functions
+*/
+
+const Array = ["superman", "spiderman", "Ironman", "Batman"]
+
+// console.table(Array)
+
+const Object = {name:'Shashi Kumar Jha', Email:'shashi@gmail.com', Contact:'7011665510'}
+
+const Object1 = [{name:'Shashi Kumar Jha', Email:'shashi@gmail.com', Contact:'7011665510'}]
+
+const Object2 = [{name:'Shashi Kumar Jha', Email:'shashi@gmail.com', Contact:'7011665510'},
+{name:'Amar Kumar', Email:'amar@gmail.com', Contact:'8787845411'},
+{name:'Afroz Zeelani', Email:'shashi@gmail.com', Contact:'1415454154'}]
+console.table(Object);
+console.table(Object1);
+console.table(Object2);
