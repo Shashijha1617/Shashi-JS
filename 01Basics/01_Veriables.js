c0d108dc6ef477ab9325b3407161748500a4a744
SHASHI KUMAR JHA <130572406+Shashijha1617@users.noreply.github.com>
2024-02-16 19:21:51 +0000

    Veriables Done Start Datatypes

---
+++ 01Basics/01_Veriables.js
@@ -0,0 +1,23 @@
+const accountId = 123456;
+let accountEmail = "shashi.jha1617@gmail.com";
+var accountPassword = "123shashi";
+accountCity = "jaipur";
+let accountState;
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState);
+
+//accountId = 123 // Account reassignment not allowed to constant
+accountEmail = "shaileshKumar@gmail.com";
+accountPassword = "21212121";
+accountCity = "Mumbai";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ])
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState);
