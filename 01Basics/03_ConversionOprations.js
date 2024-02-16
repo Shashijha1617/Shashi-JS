c4dcdd44ae0b1748486d6b654b2c06c89fa207f3
SHASHI KUMAR JHA <130572406+Shashijha1617@users.noreply.github.com>
2024-02-16 20:16:20 +0000

    ConversionOfOprators

---
+++ 01Basics/03_ConversionOprations.js
@@ -0,0 +1,74 @@
+let score = "143"
+
+console.log(typeof score);
+console.log(typeof(score));
+console.log(score);
+
+
+let valueInNumber1 = Number(score)
+console.log(valueInNumber1);
+console.log(typeof valueInNumber1);
+
+
+let score2 = "143abc"
+
+console.log(typeof score2);
+console.log(typeof(score2));
+console.log(score2);
+
+
+let valueInNumber2 = Number(score2)
+console.log(valueInNumber2);
+console.log(typeof valueInNumber2);
+
+
+let score3 = null
+
+console.log(typeof score3);
+console.log(typeof(score3));
+console.log(score3);
+
+
+let valueInNumber3 = Number(score2)
+console.log(valueInNumber3);
+console.log(typeof valueInNumber3);
+
+let score4 = true
+
+console.log(typeof score4);
+console.log(typeof(score4));
+console.log(score4);
+
+
+let valueInNumber4 = Number(score4)
+console.log(valueInNumber4);
+console.log(typeof valueInNumber4);
+
+let score5 = undefined
+
+console.log(typeof score5);
+console.log(typeof(score5));
+console.log(score5);
+
+
+let valueInNumber5 = Number(score5)
+console.log(valueInNumber5);
+console.log(typeof valueInNumber5);
+
+
+let isLogedin  =  true
+
+console.log(typeof isLogedin);
+console.log(typeof(isLogedin));
+console.log(isLogedin);
+
+let LogBoolean = Boolean(isLogedin)
+console.log(LogBoolean);
+
+// 1 or greater Number  => true  
+// 0  => false
+// "" => false
+// "Shashi" => true
+// Nan => false
+// Undefined => false
+// null => false
