f185cef3fec3e947a7da9537fd350895e3ab8a12
SHASHI KUMAR JHA <130572406+Shashijha1617@users.noreply.github.com>
2024-02-18 10:05:09 +0000

    Maths

---
+++ 01Basics/09_Maths.js
@@ -0,0 +1,21 @@
+
+console.log(Math.abs(-500));
+console.log(Math.round(325.95));
+console.log(Math.ceil(325.230));
+console.log(Math.floor(325.630));
+console.log(Math.min(2,4,6,8,10));
+console.log(Math.max(2,4,6,8,10));
+console.log(Math.sqrt(3));
+console.log(Math.pow(2,10));
+
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+console.log((Math.floor(Math.random()*10) + 1));
+
+let min = 1
+let max = 6 
+
+console.log(Math.floor(Math.random()* ((max - min) + 1)) + min); 
+// To Genereate Random Value Min Value = 1 and Max Value = 6
