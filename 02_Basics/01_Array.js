776ff7682bd08567a92b386946ffcb22d96282cc
SHASHI KUMAR JHA <130572406+Shashijha1617@users.noreply.github.com>
2024-02-18 12:59:28 +0000

    Array Part 1

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,33 @@
+// ARRAY
+
+let myArray1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+myArray1
+
+let myArray2 = new Array("Shashi", "Rohit", "Vishal", "Ronak")
+
+console.log(myArray2);
+console.log(myArray2[0]);
+console.log(myArray2[3]);
+console.log(myArray2[0]);
+console.log(myArray2.length);
+console.log(myArray2.concat(myArray1));
+
+myArray2.push("Kapil")
+myArray2
+myArray2.pop()
+myArray2
+myArray2.unshift("Kirtika")
+myArray2
+myArray2.shift()
+myArray2
+myArray1
+
+let sliceArr = myArray1.slice(1, 3)
+sliceArr
+myArray1
+
+let spliceArr = myArray1.splice(1, 3)
+spliceArr
+myArray1
+
