07ccd6809895916056444854173406cf176017eb
SHASHI KUMAR JHA <130572406+Shashijha1617@users.noreply.github.com>
2024-02-18 09:39:12 +0000

    Number Completed

--- 01Basics/08_Numbers.js
+++ 01Basics/08_Numbers.js
@@ -1,2 +1,19 @@
+const Score = 400
+Score
+ 
+const NumberVal =  new Number(800)
+console.log(NumberVal);
+console.log(NumberVal.toString());
+console.log(NumberVal.toFixed(3));
+console.log(NumberVal.toPrecision());
+console.log(NumberVal);
+
+let Number1 = 1050.98542
+
+console.log(Number1.toExponential(3));
+console.log(Number1.toPrecision(5));
 
 
+let Number2 = 5000000
+console.log(Number2.toLocaleString("en-IN"));
+console.log(Number2.toLocaleString());
