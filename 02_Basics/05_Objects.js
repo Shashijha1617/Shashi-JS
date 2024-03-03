a05f993b1bae55ffff5d96243ae234d5e5a7180f
Shashi Kumar Jha <130572406+Shashijha1617@users.noreply.github.com>
2024-03-03 17:31:26 +0000

    Objects

---
+++ 02_Basics/05_Objects.js
@@ -0,0 +1,12 @@
+const course = {
+    courseName: 'Complete Mern Stack',
+    courseFess:'2999',
+    courseIinstructure:'Shashi'
+}
+console.log(course.courseIinstructure);
+
+const {courseIinstructure} = course
+const {courseFess:fees} = course
+
+courseIinstructure
+fees
