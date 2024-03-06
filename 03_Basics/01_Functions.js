8f48e3162d4e846a9b085210a09b3d4953cc4d11
Shashijha1617 <shashi.jha1617@gmail.com>
2024-03-06 22:41:04 +0530

    Function

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -1,33 +1,54 @@
-//sayMyName()
+// //sayMyName()
 
+// function add(num1, num2 , num3){
+//     console.log(num1 + num2 + num3);
+// }
 
-function add(num1, num2 , num3){
-    console.log(num1 + num2 + num3);
+// add(1, 5, 10)
+// add(1, 5, "10")
+// add("1", 5, 10)
+// add(1, 5, null)
+// add(null, 5, 10)
+// add(undefined, 5, 10)
+// // add(number, 5, 10)
+
+// const result =  add(1,5,15)
+
+// console.log("result :",  result);
+
+// function Checkloggedin(username){
+//     if (username === undefined) {
+//            console.log("User Name is Not Defined ");
+//            return
+//     }
+//     if (username === "Shashi") {
+//            console.log("Namaskar Malik");
+//            return
+//     }
+//     return `${username} is Loggedin`
+// }
+
+// console.log(Checkloggedin("Shashi"));
+
+const user = {
+  useName: "Shashi Jha",
+  age: 29,
+};
+
+function handleObject(anyObject) {
+  console.log(`User Name is ${anyObject.useName} and age is ${anyObject.age}`);
 }
 
-add(1, 5, 10)
-add(1, 5, "10")
-add("1", 5, 10)
-add(1, 5, null)
-add(null, 5, 10)
-add(undefined, 5, 10)
-// add(number, 5, 10) 
+handleObject(user);
 
-const result =  add(1,5,15)
+const myNewArray = [200, 400, 600, 800];
 
-console.log("result :",  result);
-
-
-function Checkloggedin(username){
-    if (username === undefined) {
-           console.log("User Name is Not Defined "); 
-           return
-    }
-    if (username === "Shashi") {
-           console.log("Namaskar Malik"); 
-           return
-    }
-    return `${username} is Loggedin`
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+function returnLastValue(getArray) {
+  return getArray[getArray.length - 1];
 }
 
-console.log(Checkloggedin("Shashi"));
+console.log(returnSecondValue(myNewArray));
+console.log(returnLastValue(myNewArray));
