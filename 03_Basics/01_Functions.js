ecd4217fc00a6ce67952e06bfdc205d6bce28f0c
Shashi Kumar Jha <130572406+Shashijha1617@users.noreply.github.com>
2024-03-06 17:21:18 +0000

    dca

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -7,7 +7,6 @@ function add(num1, num2 , num3){
 
 add(1, 5, 10)
 add(1, 5, "10")
-add(1, "5", 10)
 add("1", 5, 10)
 add(1, 5, null)
 add(null, 5, 10)
@@ -17,3 +16,18 @@ add(undefined, 5, 10)
 const result =  add(1,5,15)
 
 console.log("result :",  result);
+
+
+function Checkloggedin(username){
+    if (username === undefined) {
+           console.log("User Name is Not Defined "); 
+           return
+    }
+    if (username === "Shashi") {
+           console.log("Namaskar Malik"); 
+           return
+    }
+    return `${username} is Loggedin`
+}
+
+console.log(Checkloggedin("Shashi"));
